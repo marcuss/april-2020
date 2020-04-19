c0c5e1c36a8c5ffbf881e03f402983fa63fe9306
marcuss <m4rkuz@gmail.com>
2020-04-18 21:34:29 -0500

    Program reads the file line by line.

--- bowling/src/main/java/me/marcuss/bowling/BowlingApp.java
+++ bowling/src/main/java/me/marcuss/bowling/BowlingApp.java
@@ -1,11 +1,22 @@
 package me.marcuss.bowling;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class BowlingApp {
 
-    public static void main (String args[]) {
+    public static void main(String args[]) {
         if (args == null || args.length == 0) {
             throw new IllegalArgumentException("No input file");
         }
-        System.out.println("hola mundo");
+        String fileName = args[0];
+        // read file into stream, try-with-resources
+        try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
+            stream.forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
